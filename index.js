c4b7891e67c60b14a489decc387d854f6592008f
onanmco <mustafa.onan@orioninc.com>
2022-07-22 13:45:30 +0300

    http module example post request

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const axios = require("axios");
+const http = require("http");
 
 
-axios.get("https://jsonplaceholder.typicode.com/posts")
-    .then(response => {
-        console.log(response.data);
-    });
+http.createServer((request, response) => {
+    
+    let chunks = [];
+    if (request.url == '/hello') {
+        request.on("data", chunk => {
+            chunks.push(chunk);
+        });
+        request.on("end", () => {
+            chunks = Buffer.concat(chunks);
+            chunks = chunks.toString("utf-8");
+            chunks = JSON.parse(chunks);
+            response.statusCode = 200;
+            response.end(`Hello ${chunks.name}`);
+        })
+    } else {
+        response.statusCode = 404;
+        response.end();
+    }
+})
+.listen(3000, () => {
+    console.log("server is running on port number 3000.");
+})
